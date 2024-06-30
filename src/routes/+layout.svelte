3438504f9db5496813b2f73d6989b507565f9ce5
Frail Bongat <frail.bongat@gmail.com>
2024-06-30 22:09:32 +0800

    feat: initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { ModeWatcher } from 'mode-watcher';
+	import { SiteHeader } from '$lib/components/index.js';
+	import '../app.css';
+</script>
+
+<ModeWatcher />
+<SiteHeader />
+<slot></slot>
